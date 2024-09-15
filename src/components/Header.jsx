ada7d8e15b662026448d001520836ad7a91c7415
Bharath Srinath <bharathsrinath2110@gmail.com>
2024-09-15 18:14:21 +0530

    reinitialised firebase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   
   const dispatch = useDispatch();
   
-  const loginPath = path.pathname === "/login";
+  const homePath = path.pathname === "/";
   const browsePath = path.pathname === "/browse";
 
   const user = useSelector((store) => store.user);
@@ -29,16 +29,16 @@ const Header = () => {
   return (
     <div className="absolute inset-0 mx-auto z-30 sm:w-4/5 md:px-8 py-2 h-[15vh] flex items-center justify-between">
       <img className="w-48" src={netflixLogo} alt="Netflix Logo" />
-      {!loginPath && !browsePath && (
+      {homePath && (
         <Link to={"/login"}>
           <button className="bg-red-700 text-white font-bold py-1 px-4 mx-4 rounded">
             Sign In
           </button>
         </Link>
       )}
-      {user && (
+      {browsePath && (
         <div className="flex">
-          <img className="w-8 h-8" src={user.photoURL} alt="user icon" />
+          <img className="w-8 h-8" src={user?.photoURL} alt="user icon" />
           <Link to={"/"}>
           <button onClick={() => handleLogout()} className="bg-red-700 text-white font-bold py-1 px-4 mx-4 rounded">
             Logout
